906d9b1f2a6569f1b68f27c99e68538eb9ad0500
Eli Cohen <cohen.elijahlev@gmail.com>
2023-04-20 11:35:26 -0400

    hero mvp

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import '../mystyles.scss'; //added line
 
+import HeroSection from './Home/Hero';
 import Login from './Auth/Login';
 
 const App = () => {
 	return (
 		<div className="App bg-gray-200">
 			<Routes>
+				<Route path="/" element={<HeroSection />} />
 				<Route path="/login" element={<Login />} />
 			</Routes>
 		</div>
---
+++ src/Components/Home/Hero.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Hero, Columns, Heading, Content, Image } from 'react-bulma-components';
+
+const HeroSection = () => {
+	return (
+		<Hero size="medium">
+			<Hero.Body>
+				<Columns vCentered>
+					<Columns.Column size="two-thirds">
+						<Heading textColor="primary" textFamily="secondary" textTransform="uppercase" renderAs="h1" textSize={2}>
+							Say hello to effortless package tracking
+						</Heading>
+						<Content renderAs="p" textSize={4}>
+							Say goodbye to the hassle of searching your inbox for tracking links. Add all the packages you're
+							expecting to your PackageMaster profile and easily view their status in one place.
+						</Content>
+					</Columns.Column>
+					<Columns.Column>
+						<Image src="../../../static/assets/hero.png" />
+					</Columns.Column>
+				</Columns>
+			</Hero.Body>
+		</Hero>
+	);
+};
+
+export default HeroSection;
